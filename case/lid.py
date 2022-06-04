33d3bbcdc8321c7978a9cb9b30e5c72a46eaa158
Torsten Martinsen <torsten@bullestock.net>
2022-06-04 14:29:19 +0200

    Update to ESP-32 version.

--- case/lid.py
+++ case/lid.py
@@ -11,6 +11,7 @@ from solid.utils import *
 from utils import *
 
 SEGMENTS = 64
+SEGMENTS_SHELL = SEGMENTS*4
 
 e = 0.001
 dia = 59
@@ -18,60 +19,61 @@ lid_w = 36
 corner_r = 3
 lid_th = 2
 
-pod_iw = 28
-pod_ow = 32
-pod_h = 22
-pod_x = 2
-
 cutout_h = 7
 cutout_w = 12
 cutout_offset = -1 # Offset from center
 
-# Wemos
-wm_l = 34.5
-wm_w = 25.5
-wm_th = 2
-wm_usb_offset = 1 # USB connector is not centered
+# Esp
+esp_l = 39.5
+esp_w = 31.5
+esp_th = 2
+esp_usb_offset = 0 # USB connector is centered on ESP32mini
 
 filler_offset = 10
+filler_indent = 3
 
-led_y = -5
-
-pod_l = 15
+# Pod
+pod_l = 17
 pod_l2 = 22
-pod_extra_w = 5
-pod_h = 12
-pod_h2 = 7
+pod_l3 = 11
+pod_h = 14
+pod_h2 = 11
 wt = 2
+outer_w = lid_w - filler_indent
 
 def pod():
-    pod_outer = trans(pod_l2/2, 0, -pod_h2, roundccube(pod_l + pod_l2, wm_w + pod_extra_w, pod_h + pod_h2, 1.5))
+    r = 1.5
+    pod_outer = (trans(0, 0, pod_h2, roundcube(pod_l + pod_l2, outer_w, pod_h, r)) +
+                 trans(0, 0, 0, roundcube(pod_l + pod_l3, outer_w, pod_h2 + 2*r, r)))
     inner_w = pod_h + pod_h2 - 4*wt
-    pod_inner = trans(-8, 0, -inner_w-1, roundccube(pod_l2, wm_w, inner_w, 1.5))
+    pod_inner = trans(-r, 2, 3.5, roundcube(pod_l2, esp_w - 3, inner_w, r))
     usb_w = 12
     usb_h = 8
-    usb_hole = ccube(pod_l + 5, usb_w, usb_h)
-    pod = trans(-(wm_l + 2*wt + pod_l)/2, 0, -pod_h/2, down(1)(pod_outer) - trans(1, wm_usb_offset, (pod_h - usb_h)/2, hole()(usb_hole)))
+    esp_offset_x = esp_l/2 - 16 + 2
+    esp_offset_y = outer_w/2 - .3
+    esp_offset_z = 9-1
+    usb_hole = trans(esp_l - esp_offset_x - 4, outer_w/2, 12, ccube(pod_l2, usb_w, usb_h))
+    pod = pod_outer - trans(1, -esp_usb_offset, (pod_h - usb_h)/2, hole()(usb_hole))
 
     # Hole for power plug (JST PH)
     pwr_plug_l = 5
-    pwr_plug_x = -(wm_l + pod_l)/2 - pwr_plug_l - 1
-    pwr_plug_y = 10
-    pwr_plug_z = -12
+    pwr_plug_x = pod_l + pod_l3
+    pwr_plug_y = 14
+    pwr_plug_z = 3
     es = .5 # empirics
     pwr_plug_w = 4.7 + es
     pwr_plug_h = 6 + es
     pwr_wire_sz = 4
-    pwr_wirehole = trans(pwr_plug_x + 4, pwr_plug_y, pwr_plug_z + (pwr_plug_h - pwr_wire_sz)/2, ccube(pod_l + 3, pwr_wire_sz, pwr_wire_sz))
+    pwr_wirehole = trans(pwr_plug_x - 10, pwr_plug_y, pwr_plug_z + (pwr_plug_h - pwr_wire_sz)/2, ccube(pod_l + 10, pwr_wire_sz, pwr_wire_sz))
     pwr_plughole = trans(pwr_plug_x - 1, pwr_plug_y, pwr_plug_z, ccube(pwr_plug_l + 1, pwr_plug_w, pwr_plug_h))
-    pwr_plugtube = trans(pwr_plug_x, pwr_plug_y, pwr_plug_z-1.5, ccube(pwr_plug_l, pwr_plug_w+3, pwr_plug_h+3))
+    pwr_plugtube = trans(pwr_plug_x, pwr_plug_y, pwr_plug_z-r, ccube(pwr_plug_l, pwr_plug_w+3, pwr_plug_h+3))
 
     # Hole for door switches (2.54 mm header)
-    sw_plug_x = -(wm_l + 2*wt + pod_l - 2)/2
-    sw_plug_y = -10
-    sw_plug_l = pod_l - 2
-    sw_plug_z = -12
-    es = 0.6 # empirics
+    sw_plug_x = pod_l
+    sw_plug_y = 2.5
+    sw_plug_l = pod_l + pod_l3
+    sw_plug_z = 3
+    es = 1 # empirics
     n = 2
     sw_hdr_w = n*2.5 + es
     sw_hdr_h = n*2.5 + es
@@ -79,23 +81,42 @@ def pod():
     sw_housing_w = n*2.6 + es
     sw_housing_h = n*2.6 + es
     sw_housing_l = 10
-    sw_hdrhole = trans(sw_plug_x - 1, sw_plug_y, sw_plug_z, ccube(sw_plug_l + 10, sw_hdr_w, sw_hdr_h))
-    sw_housinghole = trans(sw_plug_x - (sw_plug_l - sw_housing_l) - 1, sw_plug_y, sw_plug_z - (sw_housing_h - sw_hdr_h)/2, ccube(sw_housing_l, sw_housing_w, sw_housing_h))
-    sw_hdrtube = trans(sw_plug_x, sw_plug_y, sw_plug_z-1.5, ccube(sw_plug_l, sw_hdr_w+3, sw_hdr_h+3))
-    holes = hole()(sw_hdrhole + sw_housinghole + pwr_wirehole + pwr_plughole + pod_inner)
-    return pod - holes
+    sw_hdrhole = trans(sw_plug_x, sw_plug_y, sw_plug_z, cube([sw_plug_l, sw_hdr_w, sw_hdr_h]))
+    # LED hole
+    led_x = pod_l
+    led_y = 26
+    led_z = 7
+    led_l = 50
+    ledhole = trans(led_x, led_y, led_z, rot(90, 0, 90, cylinder(d = 5, h = 50)))
+    ledtube = trans(led_x + 2, led_y, led_z, rot(90, 0, 90, cylinder(d = 7, h = 5)))
 
-def wemos_holder():
+    holes = sw_hdrhole + pwr_wirehole + pwr_plughole + pod_inner + ledhole + ledtube
+
+    esph = rot(0, 0, 180, esp_holder())
+    part2 = trans(esp_offset_x, esp_offset_y, esp_offset_z, esph)
+
+    return pod - hole()(holes) + part2
+
+def esp_holder():
     es = .1 # empirics
-    frame_inner = ccube(wm_l+es, wm_w+es, wm_th+e)
-    frame_outer = roundccube(wm_l + 2*wt, wm_w + 2*wt, 2*wm_th, 1)
-    frame_cut = trans(-wm_l/2, wm_usb_offset, -wm_th, ccube(5, 12, wm_th+2))
-    return trans(-1, 0, 0, frame_outer - hole()(frame_inner + frame_cut))
+    frame_inner = trans(0, 0, -5, ccube(esp_l+es, esp_w+es, esp_th+e+5))
+    frame_outer = roundccube(esp_l + 2*wt, esp_w + wt, 2*esp_th, 1)
+    frame_cut = trans(-esp_l/2, esp_usb_offset, -esp_th, ccube(5, 12, esp_th+2))
+    groove_d = 3
+    groove_c = rot(90, 0, 90, cylinder(d = groove_d, h = esp_l))
+    groove_w = 10
+    dd = (groove_w-groove_d)/2
+    groove = trans(-esp_l/2, 0, groove_d/2, hull()(trans(0, dd, 0, groove_c) +
+                                                   trans(0, -dd, 0, groove_c)))
+    grooves = trans(0, esp_w/2 - groove_w/2, 0, groove) + trans(0, -(esp_w/2 - groove_w/2), 0, groove)
+    a = frame_outer - hole()(frame_inner + frame_cut + grooves)
+    return trans(-1, 0, pod_h2 + 1, a)
 
 # Depression for switch mode power supply
 def smps():
-    w = 17
-    l = 22
+    ex = .5 # Extra room
+    w = 17 + ex
+    l = 22 + ex
     h = 2
     wt = 1
     inner = ccube(w, l, h + e)
@@ -104,7 +125,7 @@ def smps():
 
 def shell_outer():
     # Hollow cylinder
-    outer = cylinder(d = dia, h = lid_w, segments = 256)
+    outer = cylinder(d = dia, h = lid_w, segments = SEGMENTS_SHELL)
     # Remove part of the cylinder that we don't need
     cutter = trans(0, dia/2, -1, ccube(dia + 2, dia, lid_w + 2))
     # Make round corners
@@ -117,16 +138,12 @@ def shell_outer():
     # Inner slit at edges
     slit1 = trans(dia/2 - 2, adj, -1, ccube(2, 2.5, lid_w + 2))
     slit2 = trans(-(dia/2 - 2), adj, -1, ccube(2, 2.5, lid_w + 2))
-    # LED hole
-    led_x = dia/2 - 5
-    ledhole = trans(dia/2 - 5, led_y, 30, rot(90, 0, 90, cylinder(d = 5, h = 10)))
-    ledtube = trans(dia/2 - 4, led_y, 30, rot(90, 0, 90, cylinder(d = 7, h = 4)))
-    return outer - cutter - rounder1 - rounder2 - slit1 - slit2 + ledtube - ledhole
+    return outer - cutter - rounder1 - rounder2 - slit1 - slit2
 
 def shell_block():
     # Block for screw hole
-    filler_h = wm_w+4
-    filler = up((lid_w - filler_h)/2)(cylinder(d = dia - 2*lid_th, h = filler_h) -
+    filler_h = lid_w -  filler_indent
+    filler = up(filler_indent)(cylinder(d = dia - 2*lid_th, h = filler_h) -
                                       trans(-dia/2, -dia/2 + filler_offset, -2, cube([dia, dia, lid_w+4])))
     screwhole = trans(0, -dia/2 + 4.8, -lid_w/2, cylinder(d = 4.5, h = 2*lid_w))
     cutter = trans(0, dia/2, -1, ccube(dia + 2, dia, lid_w + 2))
@@ -138,14 +155,13 @@ def shell_inner():
 
 def assembly():
     sh = shell_outer()
-    wmh = wemos_holder()
     part1 = sh + smps() - shell_inner() + shell_block()
-    wemos_offset_x = -15
-    wemos_offset_y = -15
-    part2 = trans(wemos_offset_x, wemos_offset_y, lid_w/2, rot(180+90, 0, 90, wmh))
-    pod_t = trans(wemos_offset_x, wemos_offset_y, lid_w/2, rot(180+90, 0, 90, pod())) - shell_inner()
-    #return pod_t + part2
-    return part1 + part2 + pod_t
+    pod_offset_x = -3.2
+    esp_offset_y = -12
+    pod_t = trans(pod_offset_x, esp_offset_y,
+                  (lid_w - outer_w)/1,
+                  rot(180+90, 180, 90, pod()))
+    return part1 + pod_t
 
 if __name__ == '__main__':
     a = assembly()
