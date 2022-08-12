6bd3511a22ce27b87e89f8cc6e00256e939ee6b7
Torsten Martinsen <torsten@bullestock.net>
2022-08-12 16:31:26 +0200

    Fillet.

--- case/magnetconnholder1.py
+++ case/magnetconnholder1.py
@@ -12,17 +12,23 @@ sh_h = 3
 
 res = (cq.Workplane("XY")
        .tag("bot")
+       # body
        .slot2D(l, w, 0)
        .extrude(h)
+       .edges(">Z")
+       .fillet(0.75)
        .workplaneFromTagged("bot")
+       # slot for connector
        .slot2D(slot_l, slot_w, 0)
        .cutThruAll()
        .workplaneFromTagged("bot")
        .transformed(offset=(0, 0, h))
+       # screw holes
        .rarray(l - 2*sh_d, 1, 2, 1)
        .cboreHole(screw_d, sh_d, sh_h)
        .workplaneFromTagged("bot")
        .transformed(offset=(0, 0, 0), rotate=(90, 0, 0))
+       # wire slot
        .slot2D(5, 3, 0)
        .cutBlind(20)
       )
