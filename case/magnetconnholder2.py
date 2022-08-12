9d0f56dd5727695a8450db7a5ae582d6be464dd1
Torsten Martinsen <torsten@bullestock.net>
2022-08-12 16:31:15 +0200

    Enlarge wire channel.

--- case/magnetconnholder2.py
+++ case/magnetconnholder2.py
@@ -10,43 +10,52 @@ h2 = 11.4
 
 screw_d = 3.5
 sh_d = 5.5
-sh_h = 3
+sh_h = 3+10
 
 # channel for wire
-ch_w = 5
-ch_h = 2
+ch_w = 7
+ch_h = 4
 
 res = (cq.Workplane("XY")
        .tag("bot")
+       # large block
        .box(l, 3*w, h1+lid_h, centered=(True, True, False))
        .edges("|Z")
        .fillet(w/2)
+       .edges("<Z")
+       .fillet(2)
        .workplaneFromTagged("bot")
        .transformed(offset=(0, w, h1+lid_h))
+       # slot for connector
        .slot2D(slot_l, slot_w, 0)
        .cutBlind(-h1)
        .workplaneFromTagged("bot")
        .transformed(offset=(0, -w, h1))
        .tag("base")
+       # small block
        .box(l, w, h2, centered=(True, True, False))
        .edges("|Z")
        .fillet(w/2-0.1)
        .workplaneFromTagged("base")
        .transformed(offset=(0, 0, -h1), rotate=(0, 180, 0))
+       # screw holes
        .rarray(l - 2*sh_d, 1, 2, 1)
        .cboreHole(screw_d, sh_d, sh_h)
        .workplaneFromTagged("bot")
        .transformed(rotate=(90, 0, 0))
-       .transformed(offset=(0, 2, -w))
+       .transformed(offset=(0, 3, -w))
+       # wire channel part 1: connector to small block
        .slot2D(ch_w, ch_h, 0)
        .cutBlind(2*w)
        .workplaneFromTagged("base")
-       .transformed(offset=(0, 0, -h1/2-ch_h))
+       .transformed(offset=(0, 0, -h1/2-ch_h/2))
+       # wire channel part 2: through small block
        .slot2D(ch_w, ch_h, 0)
        .cutBlind(3*w)
        .workplaneFromTagged("base")
        .transformed(offset=(0, 0, h2+lid_h/2), rotate=(90, 0, 0))
-       .slot2D(ch_w, ch_h*2, 0)
+       # wire channel part 3: exit through base of small block
+       .slot2D(ch_w, ch_h, 0)
        .cutBlind(w)
       )
 
