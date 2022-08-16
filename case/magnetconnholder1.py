241020db3314dc45722d95ca8b6a0bfb2dbc175f
Torsten Martinsen <torsten@bullestock.net>
2022-08-16 18:00:37 +0200

    Apply final touches.

--- case/magnetconnholder1.py
+++ case/magnetconnholder1.py
@@ -1,10 +1,10 @@
 import cadquery as cq
 
-slot_w = 5.6
-slot_l = 18.6
+slot_w = 5.55
+slot_l = 18.5
 l = 80
 x_offset = 12
-wire_offset = -30
+wire_offset = -28
 h = 6
 
 screw_d = 3.5
@@ -15,7 +15,7 @@ i_th = 1
 bot_clearance = 1
 i_x_clearance = 1
 i_y_clearance = 7
-w = slot_w + 2*i_th + 2*i_x_clearance + 2
+w = slot_w + 2*i_th + 2*i_x_clearance + 4
 
 inner = (cq.Workplane("XY")
          .transformed(offset=(x_offset, 0, 0))
@@ -29,10 +29,10 @@ inner = (cq.Workplane("XY")
          .slot2D(slot_l, slot_w, 0)
          .cutThruAll()
          .workplaneFromTagged("bot")
-         .transformed(offset=(-12, 2, 0), rotate=(90, 90, 0))
+         .transformed(offset=(-12, 2.5, 0), rotate=(90, 90, 0))
          # wire slot
-         .slot2D(5, 3, 0)
-         .cutBlind(5)
+         .rect(5, 3)
+         .cutBlind(6)
          )
 
 outer = (cq.Workplane("XY")
@@ -40,7 +40,7 @@ outer = (cq.Workplane("XY")
          .slot2D(l, w, 0)
          .extrude(h)
          .faces(">Z")
-         .fillet(0.75)
+         .fillet(1)
          .workplaneFromTagged("bot")
          .transformed(offset=(0, 0, h))
          # screw holes
@@ -50,27 +50,37 @@ outer = (cq.Workplane("XY")
          .transformed(offset=(x_offset, 0, 0))
          .slot2D(slot_l + 2*i_th + 2*i_y_clearance, slot_w + 2*i_th + 2*i_x_clearance, 0)
          .cutThruAll()
+         # wire slot
          .workplaneFromTagged("bot")
          .transformed(offset=(wire_offset, w/4, 0), rotate=(90, 0, 0))
-         # wire slot
-         .slot2D(5, 3, 0)
+         .rect(5, 3)
          .cutBlind(w*2)
+         # reed slot
+         .workplaneFromTagged("bot")
+         .transformed(offset=(wire_offset+2, 0, 0), rotate=(90, 90, 0))
+         .rect(7, 6)
+         .cutBlind(18)
+         # wire slot
+         .workplaneFromTagged("bot")
+         .transformed(offset=(wire_offset+18, 3, 0), rotate=(90, 90, 0))
+         .rect(2, 6)
+         .cutBlind(10)
          )
 
 res = outer + inner
-#res = inner
 
-s_th = 0.6 # spring thickness - was 1
+s_th = 0.5 # spring thickness - 0.6 ok
 s_length = i_y_clearance + s_th
 s_width = slot_w/2 + i_th + i_x_clearance
-def spring():
-    d1 = s_length/4
+def spring(sign):
+    d1 = s_length/5*sign
     d2 = 3
     sPnts = [
-        (1*d1, d2),
+        (1*d1, d2/2),
         (2*d1, 0),
         (3*d1, -d2),
-        (4*d1, 0)
+        (4*d1, 0),
+        (5*d1, d2/2)
     ]
     s = cq.Workplane("XY")
     r = s.spline(sPnts, includeCurrent=True)
@@ -78,12 +88,16 @@ def spring():
             .rect(s_th, h).sweep(r)
             )
 
-springs = (res
+s_spacing = slot_l + s_length + 2*i_th - s_th
+spring1 = (res
            .workplaneFromTagged("bot")
-           .transformed(offset=(x_offset - 0.5*s_length, 0, h/2))
-           .rarray(slot_l + s_length + 2*i_th - s_th, 1, 2, 1)
-       .eachpoint(lambda loc: spring().val().moved(loc), True)
+           .transformed(offset=(x_offset - 0.5*s_length + s_spacing/2, 0, h/2))
+           .eachpoint(lambda loc: spring(1).val().moved(loc), True))
+spring2 = (res
+           .workplaneFromTagged("bot")
+           .transformed(offset=(x_offset + 0.5*s_length - s_spacing/2, 0, h/2))
+           .eachpoint(lambda loc: spring(-1).val().moved(loc), True)
        )
-res = res + springs
+res = res + spring1 + spring2
 
 show_object(res)
